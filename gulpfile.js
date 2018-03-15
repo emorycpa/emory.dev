03596e0c0dd9684e64a1b9fc51559e615f3fd2ca
Your Name <you@example.com>
2018-03-15 12:35:34 -0400

    Fixed weird issue randomly deleting config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 /* Modules */
-var gulp            = require('gulp'),
-    path            = require('path'),
-    browserSync     = require('browser-sync').create(),
-    chalk           = require('chalk'),
-    autoprefixer    = require('autoprefixer'),
-    clean           = require('gulp-clean'),
-    concat          = require('gulp-concat'),
-    cssmin          = require('gulp-cssmin'),
-    gulpif          = require('gulp-if'),
-    postcss         = require('gulp-postcss'),
-    rename          = require('gulp-rename'),
-    sass            = require('gulp-sass'),
-    uglify          = require('gulp-uglify'),
-    nunjucks        = require('gulp-nunjucks-render'),
-    tap             = require('gulp-tap'),
-    inlineSource    = require('gulp-inline-source'),
-    htmlmin         = require('gulp-htmlmin'),
-    ts              = require('gulp-typescript');
+var 
+  gulp = require('gulp'),
+  path = require('path'),
+  browserSync = require('browser-sync').create(),
+  chalk = require('chalk'),
+  autoprefixer = require('autoprefixer'),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  cssmin = require('gulp-cssmin'),
+  gulpif = require('gulp-if'),
+  postcss = require('gulp-postcss'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  uglify = require('gulp-uglify'),
+  nunjucks = require('gulp-nunjucks-render'),
+  tap = require('gulp-tap'),
+  inlineSource = require('gulp-inline-source'),
+  htmlmin = require('gulp-htmlmin'),
+  ts = require('gulp-typescript');
 
 //Load Configuration 
 var config = require('./build-config.json');
@@ -26,13 +27,13 @@ var buildtasks = ['build:css', 'build:js', 'build:html'];
 
 //Create global functions
 function error() {
-    console.log(chalk.bold.red.apply(this, arguments));
-    return null;
+  console.log(chalk.bold.red.apply(this, arguments));
+  return null;
 }
 
 function logging() {
-    console.log(chalk.bold.apply(this, arguments));
-    return null; 
+  console.log(chalk.bold.apply(this, arguments));
+  return null;
 }
 
 function getConfig() {
@@ -48,130 +49,137 @@ function normalizePath() {
     .replace(/\\/g, "/");
 }
 
-function handleError(err){
-    error(err);
-    try {
-      this.emit('end');
-    } catch(e) {
-      console.log(e);
-      process.exit(1);
-    }
+function handleError(err) {
+  error(err);
+  try {
+    this.emit('end');
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
 }
 
-function logFileChange(event){
+function logFileChange(event) {
   logging('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
-
-gulp.task('build:css', ['build:clean:css'], function(){
+gulp.task('build:css', ['build:clean:css'], function () {
   return gulp.src(normalizePath(getConfig().css.source))
-          .on("error", handleError)
-          .pipe(gulpif(getConfig().css.cssPreprocessor === 'scss', sass()))
-          .on("error", handleError)
-          .pipe(postcss([autoprefixer]))
-          .pipe(gulpif(getConfig().css.minizine, cssmin()))
-          .pipe(rename({suffix: getConfig().css.suffix}))
-          .pipe(gulpif(getConfig().css.concat, concat('site.css')))
-          .pipe(gulp.dest(normalizePath(getConfig().css.build)))
-          .on("error", handleError);
+    .on("error", handleError)
+    .pipe(gulpif(getConfig().css.cssPreprocessor === 'scss', sass()))
+    .on("error", handleError)
+    .pipe(postcss([autoprefixer]))
+    .pipe(gulpif(getConfig().css.minizine, cssmin()))
+    .pipe(rename({
+      suffix: getConfig().css.suffix
+    }))
+    .pipe(gulpif(getConfig().css.concat, concat('site.css')))
+    .pipe(gulp.dest(normalizePath(getConfig().css.build)))
+    .on("error", handleError);
 });
 
-gulp.task('build:clean:css', function(){
-    return gulp.src(normalizePath(getConfig().css.build + '*'))
-            .pipe(gulpif(getConfig().css.clean, clean()));
+gulp.task('build:clean:css', function () {
+  return gulp.src(normalizePath(getConfig().css.build + '*'))
+    .pipe(gulpif(getConfig().css.clean, clean()));
 });
 
-gulp.task('build:js', ['build:clean:js'], function(){
+gulp.task('build:js', ['build:clean:js'], function () {
   return gulp.src(normalizePath(getConfig().js.source))
-          .on("error", handleError)
-          .pipe(gulpif(getConfig().js.jsPreprocessor === 'ts', ts()))
-          .on("error", handleError)
-          .pipe(gulpif(getConfig().js.minizine, uglify()))
-          .pipe(rename({suffix: getConfig().js.suffix}))
-          .pipe(gulpif(getConfig().js.concat, concat('site.js')))
-          .pipe(gulp.dest(normalizePath(getConfig().js.build)))
-          .on("error", handleError);
+    .on("error", handleError)
+    .pipe(gulpif(getConfig().js.jsPreprocessor === 'ts', ts()))
+    .on("error", handleError)
+    .pipe(gulpif(getConfig().js.minizine, uglify()))
+    .pipe(rename({
+      suffix: getConfig().js.suffix
+    }))
+    .pipe(gulpif(getConfig().js.concat, concat('site.js')))
+    .pipe(gulp.dest(normalizePath(getConfig().js.build)))
+    .on("error", handleError);
 });
 
-gulp.task('build:clean:js', function(){
-    return gulp.src(normalizePath(getConfig().js.build + '*'))
-            .pipe(gulpif(getConfig().js.clean, clean()));
+gulp.task('build:clean:js', function () {
+  return gulp.src(normalizePath(getConfig().js.build + '*'))
+    .pipe(gulpif(getConfig().js.clean, clean()));
 })
 
-gulp.task('build:html', ['build:clean:html'], function(){
-  
+gulp.task('build:html', ['build:clean:html'], function () {
+
   return gulp.src(normalizePath(getConfig().html.source))
-          .on("error", handleError)
-          .pipe(gulpif(getConfig().html.templateEngine === 'njk', nunjucks({
-            "path": [getConfig().html.templates]
-          })))
-          .on("error", handleError)
-          .pipe(rename({suffix: getConfig().html.suffix}))
-          .pipe(rename({extname: getConfig().html.extension}))
-          .pipe(gulp.dest(normalizePath(getConfig().html.build)))
-          .on("error", handleError);
+    .on("error", handleError)
+    .pipe(gulpif(getConfig().html.templateEngine === 'njk', nunjucks({
+      "path": [getConfig().html.templates]
+    })))
+    .on("error", handleError)
+    .pipe(rename({
+      suffix: getConfig().html.suffix
+    }))
+    .pipe(rename({
+      extname: getConfig().html.extension
+    }))
+    .pipe(gulp.dest(normalizePath(getConfig().html.build)))
+    .on("error", handleError);
 });
 
-gulp.task('build:clean:html', function(){
-    return gulp.src(normalizePath(getConfig().html.build + '*'))
-            .pipe(gulpif(getConfig().html.clean, clean()));
+gulp.task('build:clean:html', function () {
+  return gulp.src(normalizePath(getConfig().html.build + '*'))
+    .pipe(gulpif(getConfig().html.clean, clean()));
 });
 
-getConfig().static.forEach(function(staticResource){
-  
-  gulp.task('build:clean:' + staticResource.task, function(){
+getConfig().static.forEach(function (staticResource) {
+
+  gulp.task('build:clean:' + staticResource.task, function () {
     return gulp.src(normalizePath(staticResource.build + '*'))
-            .pipe(clean());
+      .pipe(clean());
   });
 
 
-  gulp.task('build:' + staticResource.task, ['build:clean:' + staticResource.task], function(){
+  gulp.task('build:' + staticResource.task, ['build:clean:' + staticResource.task], function () {
     return gulp.src(normalizePath(staticResource.source))
-            .on("error", handleError)
-            .pipe(gulp.dest(normalizePath(staticResource.build)))
-            .on("error", handleError);
+      .on("error", handleError)
+      .pipe(gulp.dest(normalizePath(staticResource.build)))
+      .on("error", handleError);
   });
   buildtasks.push('build:' + staticResource.task);
 });
 
-gulp.task('build', buildtasks , function(){
-    logging("Build done: " + new Date());
-    return gulp.src(normalizePath(getConfig().browserSync.directory));
+gulp.task('build', buildtasks, function () {
+  logging("Build done: " + new Date());
+  return gulp.src(normalizePath(getConfig().browserSync.directory));
 });
 
-gulp.task('serve', ['build'], function(){
-  if(getConfig().browserSync.enabled){
+gulp.task('serve', ['build'], function () {
+  if (getConfig().browserSync.enabled) {
     browserSync.init({
       server: {
         baseDir: normalizePath(getConfig().browserSync.directory),
         directory: true
-      } 
+      }
     });
   }
 });
 
-gulp.task('serve:reload',  function reload(done) {
-    browserSync.reload();
-    done();
+gulp.task('serve:reload', function reload(done) {
+  browserSync.reload();
+  done();
 });
 
-buildtasks.forEach(function(taskName){
-  gulp.task('serve:reload:'+ taskName, [taskName], function(done){
+buildtasks.forEach(function (taskName) {
+  gulp.task('serve:reload:' + taskName, [taskName], function (done) {
     browserSync.reload();
     done();
   })
 });
 
-gulp.task('watch:build', ['build'], function(){
+gulp.task('watch:build', ['build'], function () {
   gulp.watch(normalizePath(getConfig().css.source), ['serve:reload:build:css']).on('change', logFileChange);
   gulp.watch(normalizePath(getConfig().js.source), ['serve:reload:build:js']).on('change', logFileChange);
   gulp.watch(normalizePath(getConfig().html.source), ['serve:reload:build:html']).on('change', logFileChange);
-  if(getConfig().html.templateEngine != false && getConfig().html.templates != ''){
+  if (getConfig().html.templateEngine != false && getConfig().html.templates != '') {
     gulp.watch(normalizePath(getConfig().html.templates + '/**/*.*'), ['serve:reload:build:html']).on('change', logFileChange);
   }
 
-  getConfig().static.forEach(function(staticResource){
-    gulp.watch(normalizePath(staticResource.source), ['serve:reload:build:'+ staticResource.task]).on('change', logFileChange);
+  getConfig().static.forEach(function (staticResource) {
+    gulp.watch(normalizePath(staticResource.source), ['serve:reload:build:' + staticResource.task]).on('change', logFileChange);
   });
 });
 
@@ -180,14 +188,17 @@ gulp.task('watch:serve', ['build', 'serve', 'watch:build']);
 // Inline scripts/styles
 gulp.task('inlinesource', function () {
   return gulp.src('./build/*.html')
-      .pipe(inlinesource())
-      .pipe(gulp.dest('./build/inlined'));
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./build/inlined'));
 });
 
 // Inline scripts/styles and then minify HTML
-gulp.task('minify', function() {
+gulp.task('minify', function () {
   return gulp.src('./build/*.html')
     .pipe(inlineSource())
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
     .pipe(gulp.dest('./build/minified'));
 });
